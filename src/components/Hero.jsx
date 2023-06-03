7b754b2b274e74bd5cd061402fb891cec2cef836
Charles Wang <charlescello123@gmail.com>
2023-06-02 23:20:14 -0400

    basic website setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { logo } from '../assets';
+const Hero = () => {
+    return (
+        <header className="w-full flex justify-center items-center flex-col">
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <img
+                    src={logo}
+                    alt="sumz_logo"
+                    className="w-28 object-contain"
+                />
+            </nav>
+            <h1 className="head_text">Summarize Articles with
+            <br className="max-md:hidden"/>
+            <span className="purple_gradient">OpenAI GPT-4</span>
+            </h1>
+            <h2 className="desc">
+                Make reading articles faster with
+                Synopsz, an open-source article
+                summarizer that turns long passages
+                into short and simple summaries.
+            </h2>
+        </header>
+    )
+}
+
+export default Hero
